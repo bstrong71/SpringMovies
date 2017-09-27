c50b29cf0d9e106737fc65bba2bb84acacaf4a01
Bernard Strong <bstrong71@gmail.com>
2017-09-27 18:56:12 -0400

    initial commit

---
+++ src/main/java/com/example/GoToTheMovies/MovieController.java
@@ -0,0 +1,46 @@
+package com.example.GoToTheMovies;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Controller
+public class MovieController {
+
+    private static String base_url = "https://image.tmdb.org/t/p/w300";
+
+    @RequestMapping(path="/", method = RequestMethod.GET)
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping(path="/now-playing", method = RequestMethod.GET)
+    public String nowPlaying(Model model) {
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=be2a38521a7859c95e2d73c48786e4bb";
+
+        model.addAttribute("movies", getMovies(url));
+
+        return "now-playing";
+    }
+
+    @RequestMapping(path="/medium-popular-long-name", method = RequestMethod.GET)
+    public String mediumPopular() {
+        return "medium-popular-long-name";
+    }
+
+    public static List<Movie> getMovies(String route) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResultsPage movies = restTemplate.getForObject(route, ResultsPage.class);
+
+        for (Movie movie : movies.getResults()) {
+            movie.setPosterPath(base_url + movie.getPosterPath());
+        }
+
+        return movies.getResults();
+
+    }
+}
---
+++ src/main/java/com/example/GoToTheMovies/ResultsPage.java
@@ -0,0 +1,19 @@
+package com.example.GoToTheMovies;
+
+import java.util.List;
+
+public class ResultsPage {
+
+    private List<Movie> results;
+
+    public ResultsPage() {
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+//    public void setResults(List<Movie> results) {
+//        this.results = results;
+//    }
+}
